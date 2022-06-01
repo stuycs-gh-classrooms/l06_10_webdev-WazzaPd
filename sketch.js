6f0f4b99527305ca0bc0618a3841cbe0809c9de6
Joseph Wu <78169587+WazzaPd@users.noreply.github.com>
2022-06-01 15:11:54 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,39 @@
+var radius;
+var x;
+var y;
+var numCircles;
+var speed;
+
 function setup() {
-  // put setup code here
+  createCanvas(900,900);
+  background(255);
+  frameRate(5);
+  radius = 50;
+  x = radius/2;
+  y = radius/2;
+  numCircles = 3;
+  speed = radius;
 }
 
 function draw() {
-  // put drawing code here
+  background(255);
+  fill(0,255,255);
+  if((radius/2)+x+(radius*numCircles)==width){
+    speed=-speed;
+    y+=radius;
+  }
+  if(x<0){
+    speed=-speed;
+    y+=radius;
+  }
+  drawRow(x,y,radius,numCircles);
+  x+=speed;
+}
+
+function drawRow(x, y, radius, n) {
+  while (n >0){
+    circle(x,y,radius);
+    x+=radius;
+    n--;
+  }
 }
